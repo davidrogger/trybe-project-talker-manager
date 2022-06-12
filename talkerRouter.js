407f1d78060815df6baecc63dde22703a57c757b
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-12 14:49:48 -0300

    feat:talkerRouter
    add get json file read and response conditions to talker

--- talkerRouter.js
+++ talkerRouter.js
@@ -1,1 +1,19 @@
-// Iniciando PR
+const express = require('express');
+const fs = require('fs');
+
+const router = express.Router();
+
+const fileName = './talker.json';
+const encodingType = 'utf-8';
+
+router.get('/', (_req, res) => {
+  const fileData = JSON.parse(fs.readFileSync(fileName, encodingType));
+
+  if (fileData.length === 0) {
+    return res.status(200).json([]);
+  }
+
+  return res.status(200).json(fileData);
+});
+
+module.exports = router;
