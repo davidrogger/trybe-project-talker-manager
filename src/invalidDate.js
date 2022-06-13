8ca5eee1f1ce4104a8186391f354ca0eed8a90d2
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-13 17:06:47 -0300

    feat:invalidDate
    add function that validate a date by regex

---
+++ src/invalidDate.js
@@ -0,0 +1,11 @@
+const invalidDate = (date) => {
+  const dateRegex = /^(0[1-9]|1\d|2\d|3[01])\/(0[1-9]|1[0-2])\/(19|20)\d{2}$/;
+
+  const isInvalidDate = !dateRegex.test(date);
+
+  return isInvalidDate;
+};
+
+module.exports = invalidDate;
+
+// regex credits: https://www.codegrepper.com/code-examples/javascript/how+to+validate+date+input+in+javascript
