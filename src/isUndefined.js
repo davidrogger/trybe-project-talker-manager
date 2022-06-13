9645efd26065fc39699dd57c32761ac011eb7461
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-13 15:19:00 -0300

    feat:isUndefined
    add function to validate if an field is undefined

---
+++ src/isUndefined.js
@@ -0,0 +1,12 @@
+const { HTTP_BAD_REQUEST } = require('./defaultVariables');
+
+const isUndefined = (item, res) => {
+  const itemName = Object.keys(item).join();
+  if (!item[itemName]) {
+    res.status(HTTP_BAD_REQUEST).json({ message: `O campo ${itemName} é obrigatório` });
+    return true;
+  }
+  return false;
+};
+
+module.exports = isUndefined;
