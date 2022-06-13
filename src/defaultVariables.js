d3ade9ee3a1203748e80ea448d2e7909f6c87f66
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-13 15:16:33 -0300

    feat:defaultVariable
    add new variable

--- src/defaultVariables.js
+++ src/defaultVariables.js
@@ -1,9 +1,11 @@
 const HTTP_OK_STATUS = 200;
 const HTTP_NOT_FOUND = 404;
+const HTTP_BAD_REQUEST = 400;
 const PORT = '3000';
 
 module.exports = {
   HTTP_OK_STATUS,
   PORT,
   HTTP_NOT_FOUND,
+  HTTP_BAD_REQUEST,
 };
