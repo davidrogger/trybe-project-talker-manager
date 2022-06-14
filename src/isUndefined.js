1aa8431f49519cbf92abb2ea26da60c29b00e495
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-14 15:46:30 -0300

    feat:IsUndefined
    add condicional to ignore zero as undefined

--- src/isUndefined.js
+++ src/isUndefined.js
@@ -2,7 +2,7 @@ const { HTTP_BAD_REQUEST } = require('./defaultVariables');
 
 const isUndefined = (item, res) => {
   const itemName = Object.keys(item).join();
-  if (!item[itemName]) {
+  if (!item[itemName] && item[itemName] !== 0) {
     res.status(HTTP_BAD_REQUEST).json({ message: `O campo "${itemName}" é obrigatório` });
     return true;
   }
