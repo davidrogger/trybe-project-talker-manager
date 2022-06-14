7347fd5ddd430964f4558c75182646f62b0d20de
Davíd Roggér <davidrogger.a@gmail.com>
2022-06-14 14:18:49 -0300

    feat:talkValidation
    add validation to all fields inside talk

--- src/talkValidation.js
+++ src/talkValidation.js
@@ -4,21 +4,40 @@ const isUndefined = require('./isUndefined');
 
 const talkValidation = (req, res, next) => {
   const { talk } = req.body;
-
   if (isUndefined({ talk }, res)) return;
 
-  const { watchedAt } = talk;
+  next();
+};
 
+const watchedValidation = (req, res, next) => {
+  const { talk: { watchedAt } } = req.body;
   if (isUndefined({ watchedAt }, res)) return;
-
   if (invalidDate(watchedAt)) {
     res
     .status(HTTP_BAD_REQUEST)
     .json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
     return;
   }
+  next();
+};
+
+const rateValidation = (req, res, next) => {
+  const { talk: { rate } } = req.body;
+  if (isUndefined({ rate }, res)) return;
+
+  const rateNumber = Number(rate);
+  const minNumber = 1;
+  const maxNumber = 5;
+  const integerNumber = !Number.isInteger(rateNumber);
+  const minMaxNumber = rateNumber < minNumber || rateNumber > maxNumber;
+  if (integerNumber || minMaxNumber) {
+    res
+    .status(HTTP_BAD_REQUEST)
+    .json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+    return;
+  }
 
   next();
 };
 
-module.exports = talkValidation;
+module.exports = { talkValidation, watchedValidation, rateValidation };
